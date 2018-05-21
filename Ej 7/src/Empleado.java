e73b0f25b537e8a809e39c8e98c45ba4f2a17ce3
pmurga <pablomurga94@gmail.com>
2018-05-21 11:45:49 -0300

    Ej 7

---
+++ Ej 7/src/Empleado.java
@@ -0,0 +1,32 @@
+
+public class Empleado {
+
+	private int numero;
+	private String nombre;
+	
+	public Empleado(int n, String nombre)
+	{
+		numero =n;
+		this.nombre = nombre;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void verDatos()
+	{
+		System.out.println("Numero: " + numero + " // " + "Nombre: " + nombre);
+	}
+	
+	
+}
---
+++ Ej 7/src/PruebasEJ7.java
@@ -0,0 +1,13 @@
+
+public class PruebasEJ7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Empleado Emp = new Empleado(1, "Pepe");
+		
+		Emp.verDatos();
+		
+	}
+
+}
