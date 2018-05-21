3f4ca70773041a2ae36119833d07c4787ef078ec
pmurga <pablomurga94@gmail.com>
2018-05-21 10:04:43 -0300

    Primera clase practica 6

--- aye2/src/aye2/practica6/Lamparita.java
+++ aye2/src/aye2/practica6/Lamparita.java
@@ -1,8 +1,34 @@
 package aye2.practica6;
 
-public class Lamparita {
+public class Lamparita 
+{
 
-	// blabla
+	enum EstadoLampara {ON, OFF};
+	
+	private EstadoLampara estado;
+	
+	//private boolean estado;
+	
+	public Lamparita() 
+	{
+		estado = EstadoLampara.OFF; 
+		
+	}
+	
+	public void encender()
+	{
+		estado = EstadoLampara.ON;
+	}
+	
+	public void apagar()
+	{
+		estado = EstadoLampara.OFF;
+	}
+	
+	public EstadoLampara getEstado()
+	{
+		return estado;
+	}
 	
 	
 }
